fdd3056aa6278d6a5013628f2b8412bddfc095bc
Md. Arifur Rahman <iarif4u@gmail.com>
2023-11-20 16:24:09 +0600

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 's82_moonlit_nigh' );
+define( 'DB_NAME', 's84_bountiful_st' );
 
 /** Database username */
-define( 'DB_USER', 'u82_moonlit_nigh' );
+define( 'DB_USER', 'u84_bountiful_st' );
 
 /** Database password */
-define( 'DB_PASSWORD', '1p3XaDucMsTbNboc' );
+define( 'DB_PASSWORD', 'sQE6ciIRwuXBpKpU' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
